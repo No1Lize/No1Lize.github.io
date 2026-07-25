0286b5065318f1485abc486decf61e5c6fb83d9f
No1Lize <jack.linjie@gmail.com>
2026-07-25 16:50:36 +0800

    ui: hide crawler coverage dashboard from tracking admin

--- app/tracking/page.tsx
+++ app/tracking/page.tsx
@@ -1,11 +1,9 @@
-import { TrackingCoveragePanel } from "@/components/tracking-coverage-panel";
 import { UserTrackingLoader } from "@/components/user-tracking-loader";
 import { userTrackingConfig } from "@/lib/user-tracking";
 
 export default function TrackingPage() {
   return (
     <main className="page-shell subpage">
-      <TrackingCoveragePanel />
       <UserTrackingLoader initial={userTrackingConfig} />
     </main>
   );
